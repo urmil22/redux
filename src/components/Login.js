22be8d9ccfc2d7d0be787860dad640ff62ee4e12
urmil22 <urmilbhavsar8@gmail.com>
2022-07-07 14:09:27 +0530

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { login, logout } from '../features/user'
+
+function Login() {
+  const dispatch = useDispatch()
+  return (
+    <div>
+     <button onClick={ () => {
+      dispatch(login({name: "Urmil", age: 22, email: "@gmail.com"}));
+      }}>
+     Login
+     </button>
+     <button onClick={() => {
+      dispatch(logout()) }}>Logout
+     </button>
+    </div>
+  )
+}
+
+export default Login
